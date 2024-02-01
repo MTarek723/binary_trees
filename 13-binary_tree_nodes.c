f780ef1c307a4d41926638795773ba72ae233531
Abdallah616 <yassinabdou59@gmail.com>
2024-02-01 08:15:51 -0800

    task solved

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -13,5 +13,5 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 	nodes += ((tree->left || tree->right) ? 1 : 0);
 	nodes += binary_tree_nodes(tree->left);
 	nodes += binary_tree_nodes(tree->right);
-
+	return (nodes);
 }
